e85b8dcf7336f8655ad3ee8c1405968da523924f
smhrd1 <smhrd1@smhrd>
2024-11-22 15:34:46 +0900

    메소드 수정함

--- SpringBoot02/src/main/java/com/smhrd/TestClass.java
+++ SpringBoot02/src/main/java/com/smhrd/TestClass.java
@@ -3,7 +3,9 @@ package com.smhrd;
 public class TestClass {
 	
 	public void Test() {
+		System.out.println("2번이 작성함");
 		System.out.println("git 테스트 중");
+		System.out.println("3번이 작성함");
 	}
 	
 	
